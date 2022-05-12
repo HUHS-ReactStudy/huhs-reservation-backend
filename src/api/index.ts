966b3c055edef0355f1b20f7aeacf100f4f4c404
ordidxzero <ordidxzero@gmail.com>
2022-05-12 19:50:50 +0900

    router prefix 추가

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,7 +3,7 @@ import helmet from 'koa-helmet';
 import bodyParser from 'koa-bodyparser';
 import logger from 'koa-logger';
 
-const rootRouter = new Router();
+const rootRouter = new Router({ prefix: '/api/v1' });
 
 rootRouter.use(helmet()).use(logger()).use(bodyParser());
 
