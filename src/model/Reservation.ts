86f4ecd23e6a7dd86263324d22002a3f535c012b
ordidxzero <ordidxzero@gmail.com>
2022-05-12 21:06:35 +0900

    MongoDB Schema 및 Model 생성

---
+++ src/model/Reservation.ts
@@ -0,0 +1,39 @@
+import mongoose, { Document, Schema, Model } from 'mongoose';
+import { IUserModel } from './User';
+
+interface IReservation {
+  purpose: string;
+  year: number;
+  month: number;
+  day: number;
+  startTime: string;
+  endTime: string;
+  people: number;
+  userId: string | IUserModel;
+}
+
+// Method를 정의하는 부분
+interface IReservationDocument extends IReservation, Document {}
+
+// Static Method를 정의하는 부분
+export interface IReservationModel extends Model<IReservationDocument> {}
+
+// Schema를 생성하는 부분
+const ReservationSchema: Schema<IReservationDocument> = new Schema({
+  purpose: { type: String, required: true },
+  year: { type: Number, required: true },
+  month: { type: Number, required: true },
+  day: { type: Number, required: true },
+  startTime: { type: String, required: true },
+  endTime: { type: String, required: true },
+  people: { type: Number, required: true },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+});
+
+const Reservation = mongoose.model<IReservationDocument, IReservationModel>('Reservation', ReservationSchema);
+
+export default Reservation;
---
+++ src/model/User.ts
@@ -0,0 +1,40 @@
+import mongoose, { Document, Schema, Model } from 'mongoose';
+
+interface IUser {
+  name: string;
+  studentId: string;
+  department: string;
+}
+
+// Method를 정의하는 부분
+interface IUserDocument extends IUser, Document {}
+
+// Static Method를 정의하는 부분
+export interface IUserModel extends Model<IUserDocument> {}
+
+// Schema를 생성하는 부분
+const UserSchema: Schema<IUserDocument> = new Schema({
+  name: { type: String, required: true },
+  studentId: { type: String, required: true, unique: true },
+  department: { type: String, required: true },
+});
+
+// Method를 구현하는 부분
+// UserSchema.methods.setPassword = async function (password: string) {
+//   const hash = await bcrypt.hash(password, 10);
+//   this.hashedPassword = hash;
+// };
+
+// UserSchema.methods.checkPassword = async function (password: string) {
+//   const result = await bcrypt.compare(password, this.hashedPassword);
+//   return result;
+// };
+
+// Static Method를 구현하는 부분
+// UserSchema.statics.findByUsername = function (username: string) {
+//   return this.findOne({ username });
+// };
+
+const User = mongoose.model<IUserDocument, IUserModel>('User', UserSchema);
+
+export default User;
