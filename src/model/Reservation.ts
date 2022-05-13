00769de95c48785dbe78314149a2cd8a0d35bcc9
ordidxzero <ordidxzero@gmail.com>
2022-05-13 13:49:55 +0900

    Reservation Schema에 createdAt key 추가

--- src/model/Reservation.ts
+++ src/model/Reservation.ts
@@ -19,20 +19,23 @@ interface IReservationDocument extends IReservation, Document {}
 export interface IReservationModel extends Model<IReservationDocument> {}
 
 // Schema를 생성하는 부분
-const ReservationSchema: Schema<IReservationDocument> = new Schema({
-  purpose: { type: String, required: true },
-  year: { type: Number, required: true },
-  month: { type: Number, required: true },
-  day: { type: Number, required: true },
-  startTime: { type: String, required: true },
-  endTime: { type: String, required: true },
-  people: { type: Number, required: true },
-  userId: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
+const ReservationSchema: Schema<IReservationDocument> = new Schema(
+  {
+    purpose: { type: String, required: true },
+    year: { type: Number, required: true },
+    month: { type: Number, required: true },
+    day: { type: Number, required: true },
+    startTime: { type: String, required: true },
+    endTime: { type: String, required: true },
+    people: { type: Number, required: true },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
   },
-});
+  { timestamps: { createdAt: true } },
+);
 
 const Reservation = mongoose.model<IReservationDocument, IReservationModel>('Reservation', ReservationSchema);
 
