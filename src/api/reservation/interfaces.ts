d49e11be50753bf88d859dacc9b285c860e9facb
ordidxzero <ordidxzero@gmail.com>
2022-05-13 14:33:54 +0900

    interface를 정의하는 파일을 분리함

---
+++ src/api/reservation/interfaces.ts
@@ -0,0 +1,12 @@
+import { IReservation, IUser } from '../../model';
+
+export interface IGetMonthlyReservationsQuery {
+  year: number;
+  month: number;
+}
+
+export interface IGetDailyReservationsQuery extends IGetMonthlyReservationsQuery {
+  day: number;
+}
+
+export interface IAddReservationRequestBody extends IUser, Omit<IReservation, 'userId'> {}
