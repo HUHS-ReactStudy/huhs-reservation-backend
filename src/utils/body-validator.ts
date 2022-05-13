cc7e5a3ed44993e302445e3a56a3be52c4124f5f
ordidxzero <ordidxzero@gmail.com>
2022-05-13 14:33:24 +0900

    bodyValidator 함수의 리턴값을 변경하고 제네리타입을 추가함

--- src/utils/body-validator.ts
+++ src/utils/body-validator.ts
@@ -1,9 +1,14 @@
 import Joi from 'joi';
 
+type BodyValidatorResult<T> = {
+  error?: string;
+  value: T;
+};
+
 // 에러 메세지를 리턴한다.
-const bodyValidator = (body: any, schema: Joi.ObjectSchema<any>) => {
-  const { error } = schema.validate(body);
-  return error?.message;
+const bodyValidator = <T>(body: any, schema: Joi.ObjectSchema<any>, options?: Joi.ValidationOptions): BodyValidatorResult<T> => {
+  const { error, value } = schema.validate(body, options);
+  return { error: error?.message, value };
 };
 
 export default bodyValidator;
