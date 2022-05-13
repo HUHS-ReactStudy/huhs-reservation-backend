fe153a948dd231f482b59693c240edcdff1dbff0
ordidxzero <ordidxzero@gmail.com>
2022-05-13 13:51:47 +0900

    rootRouter에 reservationRouter추가함

--- src/api/index.ts
+++ src/api/index.ts
@@ -2,12 +2,11 @@ import Router from '@koa/router';
 import helmet from 'koa-helmet';
 import bodyParser from 'koa-bodyparser';
 import logger from 'koa-logger';
+import reservationRouter from './reservation';
 
 const rootRouter = new Router({ prefix: '/api/v1' });
 
-rootRouter.get('/', ctx => {
-  ctx.body = 'Hello World!';
-});
+rootRouter.use('/reservation', reservationRouter.routes());
 
 rootRouter.use(helmet()).use(logger()).use(bodyParser());
 
