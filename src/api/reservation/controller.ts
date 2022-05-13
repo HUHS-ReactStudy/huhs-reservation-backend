015d4c24d9a016a34f347b63caab83bd3e7ec14b
ordidxzero <ordidxzero@gmail.com>
2022-05-13 15:46:10 +0900

    API 추가

--- src/api/reservation/controller.ts
+++ src/api/reservation/controller.ts
@@ -1,20 +1,67 @@
 import { Context } from 'koa';
-import { IReservation, IUser } from '../../model';
-import Reservation from '../../model/Reservation';
-import User from '../../model/User';
+import Reservation, { IReservationDocument } from '../../model/Reservation';
+import User, { IUser } from '../../model/User';
+import addZero from '../../utils/add-zero';
 import bodyValidator from '../../utils/body-validator';
-import { addReservationSchema } from './joi-schema';
+import { IAddReservationRequestBody, IGetDailyReservationsQuery, IGetMonthlyReservationsQuery } from './interfaces';
+import { addReservationBodySchema, getDailyReservationsQuerySchema, getMonthlyReservationsQuerySchema } from './joi-schema';
 
-interface IAddReservationRequestBody extends IUser, Omit<IReservation, 'userId'> {}
-
-export const addReservation = async (ctx: Context) => {
-  const errorMessage = bodyValidator(ctx.request.body, addReservationSchema);
-  if (errorMessage) {
+export const getMonthlyReservations = async (ctx: Context) => {
+  const { error, value } = bodyValidator<IGetMonthlyReservationsQuery>(ctx.request.query, getMonthlyReservationsQuerySchema, {
+    convert: true,
+  });
+  if (error) {
     ctx.status = 400;
-    ctx.body = errorMessage;
+    ctx.body = error;
     return;
   }
-  const { name, studentId, department, ...reservationData } = <IAddReservationRequestBody>ctx.request.body;
+  const rawReservations = await Reservation.findByDate(value);
+  const extractDate = (r: IReservationDocument) => `${r.year}-${addZero(r.month)}-${addZero(r.day)}`;
+  const date = rawReservations.map(extractDate);
+  ctx.status = 200;
+  ctx.body = date;
+  return;
+};
+
+export const getDailyReservations = async (ctx: Context) => {
+  const { error, value } = bodyValidator<IGetDailyReservationsQuery>(ctx.request.query, getDailyReservationsQuerySchema, {
+    convert: true,
+  });
+  if (error) {
+    ctx.status = 400;
+    ctx.body = error;
+    return;
+  }
+  const rawReservations = await Reservation.find(value, null, { sort: { createdAt: 1, startTime: 1 } }).populate({
+    path: 'userId',
+  });
+  const reservations = rawReservations.map(({ purpose, year, month, day, startTime, endTime, people, userId }) => ({
+    purpose,
+    year,
+    month,
+    day,
+    startTime,
+    endTime,
+    people,
+    user: {
+      name: (userId as IUser).name,
+      department: (userId as IUser).department,
+    },
+  }));
+  ctx.status = 200;
+  ctx.body = reservations;
+  return;
+};
+
+// 예약되어 있는 시간대는 예약을 못하도록 막는 로직 추가할 것
+export const addReservation = async (ctx: Context) => {
+  const { error, value } = bodyValidator<IAddReservationRequestBody>(ctx.request.body, addReservationBodySchema);
+  if (error) {
+    ctx.status = 400;
+    ctx.body = error;
+    return;
+  }
+  const { name, studentId, department, ...reservationData } = value;
   try {
     let userId: any;
     const user = await User.findByStudentId(studentId);
