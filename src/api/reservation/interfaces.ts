5e1ddcf1b226883f3b99ee2cd329698d978b82e6
ordidxzero <ordidxzero@gmail.com>
2022-05-13 15:43:31 +0900

    Reservation Interface 수정

--- src/api/reservation/interfaces.ts
+++ src/api/reservation/interfaces.ts
@@ -9,4 +9,4 @@ export interface IGetDailyReservationsQuery extends IGetMonthlyReservationsQuery
   day: number;
 }
 
-export interface IAddReservationRequestBody extends IUser, Omit<IReservation, 'userId'> {}
+export interface IAddReservationRequestBody extends IUser, Omit<IReservation, 'userId' | 'createdAt' | 'updatedAt'> {}
