21785ce374c5de0436bdd049119590d369d8d742
ordidxzero <ordidxzero@gmail.com>
2022-05-13 15:46:25 +0900

    Reservation Router에 Controller 추가

--- src/api/reservation/index.ts
+++ src/api/reservation/index.ts
@@ -3,6 +3,8 @@ import * as reservationController from './controller';
 
 const reservationRouter = new Router();
 
+reservationRouter.get('/monthly', reservationController.getMonthlyReservations);
+reservationRouter.get('/daily', reservationController.getDailyReservations);
 reservationRouter.post('/', reservationController.addReservation);
 
 export default reservationRouter;
