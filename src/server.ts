4042e71e0480166d931a306eb66bba8eeff4f38a
ordidxzero <ordidxzero@gmail.com>
2022-05-13 13:52:38 +0900

    koa application에 helmet, logger, bodyparser를 추가함

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import dotenv from 'dotenv';
 dotenv.config();
 import Koa from 'koa';
+import logger from 'koa-logger';
+import helmet from 'koa-helmet';
+import bodyParser from 'koa-bodyparser';
 import rootRouter from './api';
 import './db';
 
 const app = new Koa();
+
+app.use(helmet()).use(logger()).use(bodyParser());
 
 app.use(rootRouter.routes()).use(rootRouter.allowedMethods());
 
