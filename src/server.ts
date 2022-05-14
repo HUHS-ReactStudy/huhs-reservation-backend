58722227feb6b96cca63c7d3d48eb16c6c681d6f
ordidxzero <ordidxzero@gmail.com>
2022-05-14 10:12:22 +0900

    Swagger Router 적용 및 CSP 에러 해결

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,21 @@ import Koa from 'koa';
 import logger from 'koa-logger';
 import helmet from 'koa-helmet';
 import bodyParser from 'koa-bodyparser';
+import cors from '@koa/cors';
 import rootRouter from './api';
 import './db';
+import swaggerRouter from './swagger';
 
 const app = new Koa();
 
-app.use(helmet()).use(logger()).use(bodyParser());
+app.use(helmet()).use(cors()).use(logger()).use(bodyParser());
+
+app.use(async (ctx, next) => {
+  ctx.set('Content-Security-Policy', 'self unsafe-inline');
+  await next();
+});
+
+app.use(swaggerRouter.routes());
 
 app.use(rootRouter.routes()).use(rootRouter.allowedMethods());
 
